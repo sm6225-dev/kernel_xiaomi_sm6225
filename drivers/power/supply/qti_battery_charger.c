601d2ca6fe190237c5edd182370a24bc64dcc0cd
ArianK16a <arian.kulmer@gmail.com>
2023-07-15 21:00:25 +0100

    power: supply: qti_battery_charger: Return charge counter in uAh
    Previously the charge counter was not reported in uAh while the
    charge full capacity was. The charge counter used to go down to
    1000 times as low as the max battery capacity is in uAh.
    Frameworks use this property to set the battery state and
    expects it to be in uAh. Refer to [1] and [2].
    
    Since commit "Use getUahDischarge when available" [3] AOSP
    uses the getUahDischarge method to determine the amount of
    discharge. Without this patch the value used to be 1000 times
    too low leading to the consumed power of
    AGGREGATE_BATTERY_CONSUMER_SCOPE_DEVICE becoming very low
    so that the relative consumption of all consumers got very
    high.
    
    [1]: https://github.com/LineageOS/android_frameworks_base/blob/d42ec4c0e5ad85792f526cc90d8830b02febc1b0/services/core/java/com/android/server/BatteryService.java#L579
    [2]: https://github.com/LineageOS/android_frameworks_base/blob/d42ec4c0e5ad85792f526cc90d8830b02febc1b0/services/core/java/com/android/server/am/BatteryStatsService.java#L2079
    [3]: LineageOS/android_frameworks_base@9f08c60
    
    Change-Id: I64dff6478588d991971b60bad61652f5c9f581b0
    Signed-off-by: GuidixX <guidixm@gmail.com>

--- drivers/power/supply/qti_battery_charger.c
+++ drivers/power/supply/qti_battery_charger.c
@@ -1237,6 +1237,9 @@ static int battery_psy_get_prop(struct power_supply *psy,
 	case POWER_SUPPLY_PROP_CHARGE_CONTROL_LIMIT_MAX:
 		pval->intval = bcdev->num_thermal_levels;
 		break;
+	case POWER_SUPPLY_PROP_CHARGE_COUNTER:
+		pval->intval = pst->prop[prop_id] * 1000;
+		break;
 	default:
 		pval->intval = pst->prop[prop_id];
 		break;
