7c138924960298c9950460f58b88d7c9a8a0f141
Alexander Winkowski <dereference23@outlook.com>
2025-01-27 19:06:05 +0530

    firmware_loader: Add /vendor/firmware_mnt/image
    Firmware is split between vendor and modem images. Add the modem
    mount point to allow direct loading of all possible firmware files.
    
    Change-Id: Ifbdd98684a39d8d1f4dd07326e0eb9cc639d93dc
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>
    Signed-off-by: muralivijay <muralidharan9845@gmail.com>
    Signed-off-by: GuidixX <guidixm@gmail.com>

--- drivers/base/firmware_loader/main.c
+++ drivers/base/firmware_loader/main.c
@@ -480,6 +480,7 @@ static const char * const fw_path[] = {
 	fw_path_para[8],
 	fw_path_para[9],
 	"/vendor/firmware",
+	"/vendor/firmware_mnt/image",
 	"/system/vendor/firmware",
 	"/system/etc/firmware",
 	"/lib/firmware/updates/" UTS_RELEASE,
