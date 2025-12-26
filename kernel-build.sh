805d76bf20e9b85616586828d6c9a4b642907e32
Guidix <guidixm@gmail.com>
2025-12-26 17:30:12 +0200

    script: kernel-build: Add more make flags
    Change-Id: I34ebe505eb6bf2eafc4b4dd9319062fdcd2e1800
    Signed-off-by: GuidixX <guidixm@gmail.com>

--- kernel-build.sh
+++ kernel-build.sh
@@ -66,6 +66,11 @@ function kernel_make() {
         ARCH=arm64 \
         LLVM=1 \
         LLVM_IAS=1 \
+        AR=llvm-ar \
+        NM="llvm-nm --format=bsd" \
+        OBJCOPY=llvm-objcopy \
+        OBJDUMP=llvm-objdump \
+        STRIP=llvm-strip \
         CROSS_COMPILE=aarch64-linux-gnu- \
         CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
         INSTALL_MOD_PATH=$INSTALL_MOD_PATH \
